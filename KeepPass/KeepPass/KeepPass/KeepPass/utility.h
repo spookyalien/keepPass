6cf6176ffd81e8c2dfe7209aea7291d2a3f56158
spookyalien <gameplayftw101@gmail.com>
2022-12-25 22:07:37 -0500

    Add files via upload

---
+++ KeepPass/KeepPass/KeepPass/KeepPass/utility.h
@@ -0,0 +1,10 @@
+#include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void reverse_arr(unsigned char* arr, int i, int f);
+void left_rotate(unsigned char* arr, int d, int n);
+void right_rotate(unsigned char* arr, int d, int n);
+void random_char_arr(unsigned char* arr, int n);
+void printCharArr(unsigned char* arr, int len, bool asChar);
