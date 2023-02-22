ba8892d70c95c875589cc4ef5f40ee3188263627
spookyalien <gameplayftw101@gmail.com>
2023-02-22 13:35:44 -0500

    Delete PBKDF directory

--- PBKDF/HMAC.h
+++
@@ -1,9 +0,0 @@
-#ifndef HMAC_H
-#define HMAC_H
-
-#include "SHA1.h"
-
-int HMAC(unsigned char* key, unsigned char* msg, unsigned char msg_digest_final[SHA1_HASH_SIZE]);
-unsigned char* compute_block_size_key(unsigned char* key, int* key_len);
-
-#endif
--- PBKDF/pkbdf2.h
+++
@@ -1,8 +0,0 @@
-#ifndef PKBDF2_H
-#define PKBDF2_H
-
-#include "HMAC.h"
-
-unsigned char* PKBDF2(char* pass, char* salt, char* prf, int c, int dk_len);
-
-#endif
