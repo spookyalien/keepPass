6a97f5f0fa8fdbe13c9775e58edd2c469da85ce5
spookyalien <gameplayftw101@gmail.com>
2023-08-21 12:45:38 -0400

    Add files via upload

--- PassClass.h
+++ PassClass.h
@@ -2,6 +2,7 @@
 #define PASSCLASS_H
 
 #include <fstream>
+#include <sstream>
 #include "Utility/cpputility.h"
 #include "PBKDF/pbkdf2.h"
 #include "AES/aes.h"
@@ -11,7 +12,7 @@
 #include <wx/artprov.h>
 #include <map>
 #ifndef WX_PRECOMP
-    #include <wx/wx.h>
+#include <wx/wx.h>
 #endif
 
 #define ROUND_COUNT 100000
@@ -19,136 +20,156 @@
 #define WINDOW_X 950
 #define WINDOW_Y 540
 
-class keepPassMenu: public wxApp
+#define ADD_PASS 1
+#define DEL_PASS 2
+
+
+class keepPassMenu : public wxApp
 {
-    public:
-        virtual bool OnInit();
+public:
+    virtual bool OnInit();
 };
 
-class keepPassFrame: public wxFrame
+class keepPassFrame : public wxFrame
 {
-    public:
-        keepPassFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
-    private:
-        void OnEnterKey(wxCommandEvent& event);
-        void OnClose(wxCloseEvent& event);
-        void OnExit(wxCommandEvent& event);
-        void OnAbout(wxCommandEvent& event);
-        wxListBox* listBox1 = new wxListBox(this, wxID_ANY);
-        wxMenuBar* mainMenu = new wxMenuBar();
-        
-        unsigned char* master_key_encr;
-        wxDECLARE_EVENT_TABLE();
+public:
+    keepPassFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+    wxButton* add_pass;
+    wxButton* del_pass;
+private:
+
+    void OnPassword(wxCommandEvent& event);
+    void OnEnterKey(wxCommandEvent& event);
+    void OnClose(wxCloseEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+    wxMenuBar* mainMenu = new wxMenuBar();
+
+    unsigned char* master_key_encr;
+    wxDECLARE_EVENT_TABLE();
 };
 
-enum 
+enum
 {
-    ID_OPTIONS = 99
+    ID_OPTIONS = 99,
+    BUTTON_ADD = wxID_HIGHEST + ADD_PASS,
+    BUTTON_DEL = wxID_HIGHEST + DEL_PASS
 };
+
+struct structure
+{
+    std::string word;
+    unsigned char* iv;
+    unsigned char* cipher;
+    int encr_len;
+};
+
 
 wxBEGIN_EVENT_TABLE(keepPassFrame, wxFrame)
-EVT_MENU(wxID_EXIT,  keepPassFrame::OnExit)
+EVT_MENU(wxID_EXIT, keepPassFrame::OnExit)
 EVT_MENU(wxID_ABOUT, keepPassFrame::OnAbout)
+EVT_BUTTON(BUTTON_ADD, keepPassFrame::OnPassword)
+EVT_BUTTON(BUTTON_DEL, keepPassFrame::OnExit)
 wxEND_EVENT_TABLE()
 wxIMPLEMENT_APP(keepPassMenu);
 
 
-class KeepPass
-{
-    public:
-        void add_pass(unsigned char* key);
-        void remove_pass(unsigned char* key);
-        void print_pass(unsigned char* key);
-        void reset_pass();
-        const char* pass_txt = "pass.txt";
-    private:
-        struct structure
-        {
-            std::string word;
-            unsigned char* iv;
-            unsigned char* cipher;
-            int encr_len;
-        };
-        void write_pass(structure item, unsigned char* key);
-};
-
-
-void KeepPass::write_pass(KeepPass::structure item, unsigned char* key)
-{
-    std::ofstream pass_file(pass_txt, std::ios::app);
-    const char hex[17] = "0123456789ABCDEF";
-
-    std::cin >> item.word;
-    std::string iv = generate_salt(DEFAULT_LEN);
-    auto iv_chrs = iv.c_str();
-    item.iv = reinterpret_cast<unsigned char*>(const_cast<char*>(iv_chrs));
-
-    auto pass_chrs = item.word.c_str();
-    unsigned char* tmp = reinterpret_cast<unsigned char*>(const_cast<char*>(pass_chrs));
-    
-    item.encr_len = aes_encrypt(tmp, item.word.size(), key, AES_256, AES_CBC, item.iv, &item.cipher);
-    memset(tmp, 0, item.word.size());
-    item.word.resize(item.word.capacity(), 0);
-    cleanse(&item.word[0], item.word.size());
-    item.word.clear();
-
-    for (int i = 0; i < item.encr_len; i++) {
-        pass_file << hex[item.cipher[i] >> 4] << hex[item.cipher[i] & 0x0f];
-    }
-    pass_file << item.iv << item.encr_len << std::endl;
-}
-
-void KeepPass::add_pass(unsigned char* key)
-{
-    structure site;
-    structure pass;
- 
-    printf("[+] Enter site for this password to be used: ");
-    write_pass(site, key);
-    printf("[+] Please enter the password to store: ");
-    write_pass(pass, key);
-}
-
-void KeepPass::remove_pass(unsigned char* key)
-{
-
-}
-void KeepPass::print_pass(unsigned char* key)
-{
-    std::ifstream pass_file(pass_txt, std::ios::out);
-    std::string line;
-    unsigned char* dec = NULL;
-
-    if (pass_file.is_open()) {
-        while (std::getline(pass_file, line)) {
-            std::string num = line.substr(line.size()-2, 2);
-            int len = std::stoi(num);
-            std::string iv_str = line.substr(line.size()-2-DEFAULT_LEN, DEFAULT_LEN);
-            std::string encr_pass = line.substr(0, line.size()-2-DEFAULT_LEN);
-            auto ciph_chrs = encr_pass.c_str();
-            auto iv_chrs = iv_str.c_str();
-
-            unsigned char* iv = reinterpret_cast<unsigned char*>(const_cast<char*>(iv_chrs));
-            unsigned char* cipher = reinterpret_cast<unsigned char*>(const_cast<char*>(ciph_chrs));
-
-
-            // FIXME: SEGFAULT 
-            int decr_len = aes_decrypt(cipher, line.size()-2-DEFAULT_LEN, key, AES_256, AES_CBC, iv, &dec);
-            std::cout << dec << std::endl;
-        }
-    }
-    else 
-        printf("[+] No active pass file...\n");
-
-    pass_file.close();
-}
-
-void KeepPass::reset_pass() 
-{
-    if( remove("key.asc") == 0 && remove("pass.txt") == 0)
-        puts( "File successfully deleted");
-    
-    return;
-}
+//class KeepPass
+//{
+//public:
+//    void add_pass(unsigned char* key);
+//    void remove_pass(unsigned char* key);
+//    void print_pass(unsigned char* key);
+//    void reset_pass();
+//    const char* pass_txt = "pass.txt";
+//private:
+//    struct structure
+//    {
+//        std::string word;
+//        unsigned char* iv;
+//        unsigned char* cipher;
+//        int encr_len;
+//    };
+//    void write_pass(structure item, unsigned char* key);
+//};
+//
+//
+//void KeepPass::write_pass(KeepPass::structure item, unsigned char* key)
+//{
+//    std::ofstream pass_file(pass_txt, std::ios::app);
+//    const char hex[17] = "0123456789ABCDEF";
+//
+//    std::cin >> item.word;
+//    std::string iv = generate_salt(DEFAULT_LEN);
+//    auto iv_chrs = iv.c_str();
+//    item.iv = reinterpret_cast<unsigned char*>(const_cast<char*>(iv_chrs));
+//
+//    auto pass_chrs = item.word.c_str();
+//    unsigned char* tmp = reinterpret_cast<unsigned char*>(const_cast<char*>(pass_chrs));
+//
+//    item.encr_len = aes_encrypt(tmp, item.word.size(), key, AES_256, AES_CBC, item.iv, &item.cipher);
+//    memset(tmp, 0, item.word.size());
+//    item.word.resize(item.word.capacity(), 0);
+//    cleanse(&item.word[0], item.word.size());
+//    item.word.clear();
+//
+//    for (int i = 0; i < item.encr_len; i++) {
+//        pass_file << hex[item.cipher[i] >> 4] << hex[item.cipher[i] & 0x0f];
+//    }
+//    pass_file << item.iv << item.encr_len << std::endl;
+//}
+//
+//void KeepPass::add_pass(unsigned char* key)
+//{
+//    structure site;
+//    structure pass;
+//
+//    printf("[+] Enter site for this password to be used: ");
+//    write_pass(site, key);
+//    printf("[+] Please enter the password to store: ");
+//    write_pass(pass, key);
+//}
+//
+//void KeepPass::remove_pass(unsigned char* key)
+//{
+//
+//}
+//void KeepPass::print_pass(unsigned char* key)
+//{
+//    std::ifstream pass_file(pass_txt, std::ios::out);
+//    std::string line;
+//    unsigned char* dec = NULL;
+//
+//    if (pass_file.is_open()) {
+//        while (std::getline(pass_file, line)) {
+//            std::string num = line.substr(line.size() - 2, 2);
+//            int len = std::stoi(num);
+//            std::string iv_str = line.substr(line.size() - 2 - DEFAULT_LEN, DEFAULT_LEN);
+//            std::string encr_pass = line.substr(0, line.size() - 2 - DEFAULT_LEN);
+//            auto ciph_chrs = encr_pass.c_str();
+//            auto iv_chrs = iv_str.c_str();
+//
+//            unsigned char* iv = reinterpret_cast<unsigned char*>(const_cast<char*>(iv_chrs));
+//            unsigned char* cipher = reinterpret_cast<unsigned char*>(const_cast<char*>(ciph_chrs));
+//
+//
+//            // FIXME: SEGFAULT 
+//            int decr_len = aes_decrypt(cipher, line.size() - 2 - DEFAULT_LEN, key, AES_256, AES_CBC, iv, &dec);
+//            std::cout << dec << std::endl;
+//        }
+//    }
+//    else
+//        printf("[+] No active pass file...\n");
+//
+//    pass_file.close();
+//}
+//
+//void KeepPass::reset_pass()
+//{
+//    if (remove("key.asc") == 0 && remove("pass.txt") == 0)
+//        puts("File successfully deleted");
+//
+//    return;
+//}
 
 #endif
--- keepPass.cpp
+++ keepPass.cpp
@@ -8,7 +8,7 @@ bool verify_pass(std::string master_key)
     int salt_len = 16;
     int round_count = 100000;
     const char hex[17] = "0123456789ABCDEF";
-    
+
     std::ifstream file("key.asc");
 
     // Master hash not created yet add as normal
@@ -19,7 +19,7 @@ bool verify_pass(std::string master_key)
         file << salt << std::endl;
         file << round_count << std::endl;
         file << salt_len << std::endl;
-        
+
         for (int i = 0; i < key_len; i++) {
             file << hex[master_key_encr[i] >> 4] << hex[master_key_encr[i] & 0x0f];
         }
@@ -36,19 +36,28 @@ bool verify_pass(std::string master_key)
         std::getline(file, line);
         std::cout << line << std::endl;
         auto ciph_chrs = line.c_str();
-        unsigned char* cipher = reinterpret_cast<unsigned char*>(const_cast<char*>(ciph_chrs));
+        unsigned char* cipher = new unsigned char[line.length() / 2];
 
-        
+        for (int i = 0; i < line.length(); i += 2) {
+            std::istringstream iss(line.substr(i, 2));
+            int hex_val;
+            iss >> std::hex >> hex_val;
+            cipher[i / 2] = static_cast<unsigned char>(hex_val);
+        }
+
+
         key_len = PBKDF2(reinterpret_cast<unsigned char*>(const_cast<char*>(master_key.c_str())), master_key.length(), reinterpret_cast<unsigned char*>(const_cast<char*>(salt.c_str())), salt_len, round_count, DK_LEN, &master_key_encr);
-        
+
         for (int i = 0; i < key_len; i++) {
             if (master_key_encr[i] != cipher[i]) {
                 master_key.resize(master_key.capacity(), 0);
                 cleanse(&master_key[0], master_key.size());
                 master_key.clear();
+                delete[] cipher;
                 return false;
             }
         }
+        delete[] cipher;
     }
 
 
@@ -68,7 +77,7 @@ void launch()
     printf("--------------------------------\n");
     printf("\tWelcome to KeepPass\t\n");
     printf("--------------------------------\n");
-    
+
     unsigned char* master_key = verify_pass();
     print_menu();
     while (true) {
@@ -102,22 +111,29 @@ bool keepPassMenu::OnInit()
 {
     wxDisplay disp;
     wxRect disp_rect = disp.GetClientArea();
-    keepPassFrame *frame = new keepPassFrame( "keepPass", wxPoint((disp_rect.GetWidth()-WINDOW_X)/2, (disp_rect.GetHeight()-WINDOW_Y)/2), wxSize(WINDOW_X, WINDOW_Y) );
-    frame->Show( true );
+    keepPassFrame* frame = new keepPassFrame("keepPass", wxPoint((disp_rect.GetWidth() - WINDOW_X) / 2, (disp_rect.GetHeight() - WINDOW_Y) / 2), wxSize(WINDOW_X, WINDOW_Y));
+    frame->Show(true);
     return true;
 }
 
 keepPassFrame::keepPassFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
-        : wxFrame(NULL, wxID_ANY, title, pos, size)
+    : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
-    wxTextEntryDialog* pass_input = new wxTextEntryDialog(this,"Enter master password.","keepPass",wxEmptyString,wxOK|wxTE_PASSWORD);
+    wxTextEntryDialog* pass_input = new wxTextEntryDialog(this, "Enter master password.", "keepPass", wxEmptyString, wxOK | wxTE_PASSWORD);
+    pass_input->CenterOnScreen();
     pass_input->Bind(wxEVT_TEXT_ENTER, &keepPassFrame::OnEnterKey, this, wxID_ANY);
     int result = pass_input->ShowModal();
 
     if (result == wxID_OK) {
-        if(!verify_pass(pass_input->GetValue().ToStdString()))
+        if (!verify_pass(pass_input->GetValue().ToStdString()))
             exit(0);
     }
+
+    wxFlexGridSizer* fgSizer1;
+    fgSizer1 = new wxFlexGridSizer(0, 2, 0, 0);
+    fgSizer1->SetFlexibleDirection(wxBOTH);
+    fgSizer1->SetNonFlexibleGrowMode(wxFLEX_GROWMODE_SPECIFIED);
+
     auto menuFile = new wxMenu();
     auto menuItemFileQuit = menuFile->Append(wxID_EXIT);
     menuItemFileQuit->SetBitmap(wxArtProvider::GetBitmap(wxART_QUIT, wxART_MENU));
@@ -127,20 +143,28 @@ keepPassFrame::keepPassFrame(const wxString& title, const wxPoint& pos, const wx
     auto menuHelp = new wxMenu();
     menuHelp->Append(wxID_ABOUT);
 
-    pass_input->Destroy();
+
+    add_pass = new wxButton(this, BUTTON_ADD, _T("Add Password"), wxDefaultPosition, wxDefaultSize, 0);
+    del_pass = new wxButton(this, BUTTON_DEL, _T("Remove Password"), wxDefaultPosition, wxDefaultSize, 0);
+    fgSizer1->Add(add_pass, 0, wxALL, 5);
+    fgSizer1->Add(del_pass, 0, wxALL, 5);
 
     mainMenu->Append(menuFile, "&File");
     mainMenu->Append(menuEdit, "&Edit");
     mainMenu->Append(menuHelp, "&Help");
-
-    SetMenuBar(mainMenu);
-
-
     
-    Connect(wxEVT_CLOSE_WINDOW, wxCloseEventHandler(keepPassFrame::OnClose));
+    this->SetSizer(fgSizer1);
+    this->Layout();
+    this->SetMenuBar(mainMenu); 
+    this->Connect(wxEVT_CLOSE_WINDOW, wxCloseEventHandler(keepPassFrame::OnClose));
 }
 
-void keepPassFrame::OnEnterKey(wxCommandEvent& event) 
+void keepPassFrame::OnPassword(wxCommandEvent& event)
+{
+    std::ofstream pass("pass.txt");
+}
+
+void keepPassFrame::OnEnterKey(wxCommandEvent& event)
 {
     wxTextCtrl* passwordEntry = dynamic_cast<wxTextCtrl*>(event.GetEventObject());
     wxString input = passwordEntry->GetValue();
@@ -149,13 +173,13 @@ void keepPassFrame::OnEnterKey(wxCommandEvent& event)
 
 void keepPassFrame::OnExit(wxCommandEvent& event)
 {
-    Close( true );
+    Close(true);
 }
 
 void keepPassFrame::OnAbout(wxCommandEvent& event)
 {
-    wxMessageBox( "This is a password manager that aims to keep passwords safe and accessible using AES256 and PBKDF2.",
-                  "About keepPass", wxOK | wxICON_INFORMATION );
+    wxMessageBox("This is a password manager that aims to keep passwords safe and accessible using AES256 and PBKDF2.",
+        "About keepPass", wxOK | wxICON_INFORMATION);
 }
 
 void keepPassFrame::OnClose(wxCloseEvent& event)
@@ -182,84 +206,84 @@ int main()
     }
     printf("\n");
     */
-    
 
 
-        /*      --------------SHA 1-------- -
-     #define TEST2 "xyz"
-    SHA1 sha;
-    uint8_t msg_digest[20];
-    sha = SHA1();
-    sha.update((unsigned char*)TEST2, strlen(TEST2));
-    sha.result(msg_digest);
 
-        for (int i = 0; i < 20; ++i)
-        {
-            printf("%02X ", msg_digest[i]);
-        }
-        printf("\n");
-    */
+    /*      --------------SHA 1-------- -
+ #define TEST2 "xyz"
+SHA1 sha;
+uint8_t msg_digest[20];
+sha = SHA1();
+sha.update((unsigned char*)TEST2, strlen(TEST2));
+sha.result(msg_digest);
+
+    for (int i = 0; i < 20; ++i)
+    {
+        printf("%02X ", msg_digest[i]);
+    }
+    printf("\n");
+*/
 
 
-    /*          --------------AES-------- -
-    const char* txt = "asidlhgfyiuyguaysdgdcvetwee";
-    const char* k = "abcdefghijklmnopabcdefghijklmnop";
-    const char* ivv = "zyxwvutsrqponmlk";
-    unsigned char* key = (unsigned char*)k;
-    unsigned char* text = (unsigned char*)txt;
-    unsigned char* iv = (unsigned char*)ivv;
-    unsigned char* cipher = NULL;
-    unsigned char* dec = NULL;
+/*          --------------AES-------- -
+const char* txt = "asidlhgfyiuyguaysdgdcvetwee";
+const char* k = "abcdefghijklmnopabcdefghijklmnop";
+const char* ivv = "zyxwvutsrqponmlk";
+unsigned char* key = (unsigned char*)k;
+unsigned char* text = (unsigned char*)txt;
+unsigned char* iv = (unsigned char*)ivv;
+unsigned char* cipher = NULL;
+unsigned char* dec = NULL;
 
-    int encr_len = aes_encrypt(text, 27, key, AES_256, AES_CTR, iv, &cipher);
-    int decr_len = aes_decrypt(cipher, encr_len, key, AES_256, AES_CTR, iv, &dec);
+int encr_len = aes_encrypt(text, 27, key, AES_256, AES_CTR, iv, &cipher);
+int decr_len = aes_decrypt(cipher, encr_len, key, AES_256, AES_CTR, iv, &dec);
 
-    printf("==============AES_CTR\n");
-    printf("plain text: ");
-    printCharArr(text, 27);
-    printf("Key: ");
-    printCharArr(key, 16);
-    printf("Cipher: ");
-    printCharArr(cipher, encr_len);
-    printf("Decrypted: ");
-    printCharArr(dec, decr_len);
+printf("==============AES_CTR\n");
+printf("plain text: ");
+printCharArr(text, 27);
+printf("Key: ");
+printCharArr(key, 16);
+printf("Cipher: ");
+printCharArr(cipher, encr_len);
+printf("Decrypted: ");
+printCharArr(dec, decr_len);
 
-    encr_len = aes_encrypt(text, 27, key, AES_256, AES_ECB, NULL, &cipher);
-    decr_len = aes_decrypt(cipher, encr_len, key, AES_256, AES_ECB, NULL, &dec);
+encr_len = aes_encrypt(text, 27, key, AES_256, AES_ECB, NULL, &cipher);
+decr_len = aes_decrypt(cipher, encr_len, key, AES_256, AES_ECB, NULL, &dec);
 
-    printf("\n==============AES_ECB\n");
-    printf("plain text: ");
-    printCharArr(text, 27);
-    printf("Key: ");
-    printCharArr(key, 32);
-    printf("Cipher: ");
-    printCharArr(cipher, encr_len);
-    printf("Decrypted: ");
-    printCharArr(dec, decr_len);
+printf("\n==============AES_ECB\n");
+printf("plain text: ");
+printCharArr(text, 27);
+printf("Key: ");
+printCharArr(key, 32);
+printf("Cipher: ");
+printCharArr(cipher, encr_len);
+printf("Decrypted: ");
+printCharArr(dec, decr_len);
 
-    encr_len = aes_encrypt(text, 27, key, AES_256, AES_CBC, iv, &cipher);
-    decr_len = aes_decrypt(cipher, encr_len, key, AES_256, AES_CBC, iv, &dec);
+encr_len = aes_encrypt(text, 27, key, AES_256, AES_CBC, iv, &cipher);
+decr_len = aes_decrypt(cipher, encr_len, key, AES_256, AES_CBC, iv, &dec);
 
-    printf("\n==============AES_CBC\n");
-    printf("plain text: ");
-    printCharArr(text, 27);
-    printf("Key: ");
-    printCharArr(key, 16);
-    printf("Cipher: ");
-    printCharArr(cipher, encr_len);
-    printf("Decrypted: ");
-    printCharArr(dec, decr_len);
-    free(cipher);
-    free(dec); */
+printf("\n==============AES_CBC\n");
+printf("plain text: ");
+printCharArr(text, 27);
+printf("Key: ");
+printCharArr(key, 16);
+printf("Cipher: ");
+printCharArr(cipher, encr_len);
+printf("Decrypted: ");
+printCharArr(dec, decr_len);
+free(cipher);
+free(dec); */
 
-    // unsigned char* out;
-    // int len = PBKDF2((unsigned char*) "passwordPASSWORDpassword", 24, (unsigned char*) "saltSALTsaltSALTsaltSALTsaltSALTsalt", 36, 40960, 25, &out);
-    // printf("%d\n", len);
-    // for (int i = 0; i < len; i++) {
-    //         printf("%02X ", out[i]);
-    // }
-    // printf("\n");
-    
-    
-    //launch();
+// unsigned char* out;
+// int len = PBKDF2((unsigned char*) "passwordPASSWORDpassword", 24, (unsigned char*) "saltSALTsaltSALTsaltSALTsaltSALTsalt", 36, 40960, 25, &out);
+// printf("%d\n", len);
+// for (int i = 0; i < len; i++) {
+//         printf("%02X ", out[i]);
+// }
+// printf("\n");
+
+
+//launch();
 //}
