f6f4df5911887fd052855f4a5ef1417b40b7a88d
spookyalien <gameplayftw101@gmail.com>
2024-08-06 19:05:31 -0400

    Removed mentions of unlock all button.

--- PassClass.h
+++ PassClass.h
@@ -1,42 +1,84 @@
 #ifndef PASSCLASS_H
 #define PASSCLASS_H
 
-#include <fstream>
 #include "Utility/cpputility.h"
 #include "PBKDF/pbkdf2.h"
 #include "AES/aes.h"
+#include <unordered_set>
 
 #include <wx/wxprec.h>
 #include <wx/display.h>
+#include <wx/artprov.h>
+#include <wx/textctrl.h>
+#include <wx/sizer.h>
+#include <wx/stattext.h>
+#include <wx/button.h>
+#include <map>
 #ifndef WX_PRECOMP
-    #include <wx/wx.h>
+#include <wx/wx.h>
 #endif
 
+#define ROUND_COUNT 100000
+#define DEFAULT_SALT_LEN 16
 #define WINDOW_X 950
 #define WINDOW_Y 540
-#define BOX_X 300
-#define BOX_Y 50
+#define DELIMITER '|'
+#define PASS_FILE "pass.txt"
+#define convert_uchar(strng) reinterpret_cast<unsigned char *>(const_cast<char *>(strng.c_str()))
 
-class keepPassMenu: public wxApp
+class keepPassMenu : public wxApp
 {
-    public:
-        virtual bool OnInit();
+public:
+    virtual bool OnInit();
 };
 
-class keepPassFrame: public wxFrame
+class keepPassFrame : public wxFrame
 {
-    public:
-        keepPassFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
-    private:
-        void OnHello(wxCommandEvent& event);
-        void OnEnterKey(wxCommandEvent& event);
-        void OnExit(wxCommandEvent& event);
-        void OnAbout(wxCommandEvent& event);
-        wxDECLARE_EVENT_TABLE();
+public:
+    keepPassFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+    wxButton *add_pass;
+    wxButton *del_pass;
+
+private:
+    void on_password(wxCommandEvent &event);
+    void on_delete(wxCommandEvent &event);
+    void on_close(wxCloseEvent &event);
+    void on_exit(wxCommandEvent &event);
+    void on_about(wxCommandEvent &event);
+    void unlocks();
+    int create_master(unsigned char **master_key);
+    wxMenuBar *main_menu = new wxMenuBar();
+
+    wxListBox *pass_list;
+    wxListBox *user_list;
+    wxListBox *site_list;
+    std::unordered_set<std::string> added;
+    unsigned char *master_key = NULL;
+    int key_length = 0;
+
+    wxDECLARE_EVENT_TABLE();
 };
 
 enum
 {
+    ADD_PASS = 2,
+    DEL_PASS = 3
+};
+
+enum
+{
+    ID_OPTIONS = 99,
+    BUTTON_ADD = wxID_HIGHEST + ADD_PASS,
+    BUTTON_DEL = wxID_HIGHEST + DEL_PASS
+};
+
+typedef struct pass_format
+{
+    unsigned char *iv;
+    unsigned char *cipher;
+    int len;
+} pass_format;
+
 class PassInput : public wxDialog
 {
 public:
@@ -93,5 +135,12 @@ private:
     wxTextCtrl *passInput;
 };
 
+wxBEGIN_EVENT_TABLE(keepPassFrame, wxFrame)
+    EVT_MENU(wxID_EXIT, keepPassFrame::on_exit)
+        EVT_MENU(wxID_ABOUT, keepPassFrame::on_about)
+            EVT_BUTTON(BUTTON_ADD, keepPassFrame::on_password)
+                EVT_BUTTON(BUTTON_DEL, keepPassFrame::on_delete)
+                    wxEND_EVENT_TABLE()
+                        wxIMPLEMENT_APP(keepPassMenu);
 
 #endif
